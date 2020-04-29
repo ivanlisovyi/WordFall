f92f9c718bc2dc49153eb3964092c9c5b445b4eb
ivanlisovyi <lisovyi.ivan@gmail.com>
2020-04-29 09:36:01 +0200

    Initial commit

---
+++ WordFall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordFall
+//
+//  Created by Ivan Lisovyi on 29.04.20.
+//  Copyright © 2020 Ivan Lisovyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WordFallTests/WordFallTests.swift
@@ -0,0 +1,34 @@
+//
+//  WordFallTests.swift
+//  WordFallTests
+//
+//  Created by Ivan Lisovyi on 29.04.20.
+//  Copyright © 2020 Ivan Lisovyi. All rights reserved.
+//
+
+import XCTest
+@testable import WordFall
+
+class WordFallTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
